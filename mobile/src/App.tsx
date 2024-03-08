1f8b29828130518e10cf75e14dd97f93d581f293
TERRYCHEN\timst <terrychen@daoyidh.com>
2024-03-08 15:24:32 +0800

    [新增] 單向對話

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,15 +1,23 @@
 import {useEffect, useState} from 'react'
 import Header from "./ui/Header.tsx";
 import Footer from './ui/Footer.tsx'
+
+interface MessageProps {
+    notification:string;
+    action:string;
+    talk:Array<{id:string;text:string;time:string;sendId:string;}>
+}
 function App() {
     const [member, setMember] = useState({
         action: 'login',
         id : "HDM001",
         name: 'terry',
     })
-    const [message ,setMessage] = useState({
+
+    const [message ,setMessage] = useState<MessageProps>({
         notification:'',
         action:'',
+        talk:[],
     });
 
     const [newData,setNewData] = useState('')
@@ -49,6 +57,18 @@ function App() {
                     getNewData();
                     setMessage(prevState => {return{...prevState,action: data.message}});
                 }
+                if (data.type === "talk") {
+
+                    console.log(member.id);
+                    console.log(data.sendId);
+                    const today = new Date();
+                    const formatDate = `${today.getHours().toString().padStart(2,"0")}:${today.getMinutes().toString().padStart(2,"0")}`
+                    setMessage(prevState => {
+                        return {...prevState,
+                            talk: [...prevState.talk,{id:data.id,text:data.message as string,time:formatDate,sendId:data.sendId}]
+                        }
+                    });
+                }
             }
         })
         setWs(newWs);
@@ -72,6 +92,22 @@ function App() {
                     </div>
                     <div className={" mt-4 w-full flex justify-center"}>
                         {`最後更新時間: ${newData}`}
+                    </div>
+                    <div className={" mt-4 w-full flex justify-center"}>
+                        <div className={"flex flex-col w-[80%] mb-4 overflow-y-auto h-[500px] border-b-amber-200 border-2 md:w-1/2"}>
+                            {message.talk.map((item)=>(
+                                <div className={item.sendId === member.id ? "w-full flex justify-start":"w-full flex justify-end"}>
+                                    <div className={item.sendId === member.id ? "flex justify-between w-1/2 mt-4 bg-amber-200 rounded-lg p-2 hover:bg-amber-300" : "rounded-lg p-2 flex justify-between w-1/2 mt-4 bg-pink-200 hover:bg-pink-300"}>
+                                        <div className="w-3/4 break-all">
+                                            {item.text}
+                                        </div>
+                                        <div className={"text-gray-400 hover:text-gray-600"}>
+                                            {item.time}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </main>
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import {useRef, useState} from 'react'
 import Header from "./ui/Header.tsx";
 import Footer from './ui/Footer.tsx'
 function App() {
   const [count, setCount] = useState(0)
+  const formRef = useRef<HTMLFormElement>(null);
+  const formRef2 = useRef<HTMLFormElement>(null);
 
   const testConnection = async ()=>{
     await fetch('http://localhost:4080/sendMessage',{
@@ -25,6 +27,48 @@ function App() {
     }).then(async (res)=>{
       console.log(await res.json());
     })
+  }
+
+  const talkAction = async (text:string)=>{
+    await fetch('http://localhost:4080/sendMessage',{
+      method:'post',
+      body:JSON.stringify({ id:'HDM001' ,type:"talk",message:text , sendId:'HDM002'}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(async (res)=>{
+      console.log(await res.json());
+    })
+  }
+  const talkAction2 = async (text:string)=>{
+    await fetch('http://localhost:4080/sendMessage',{
+      method:'post',
+      body:JSON.stringify({ id:'HDM001' ,type:"talk",message:text , sendId:'HDM001'}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(async (res)=>{
+      console.log(await res.json());
+    })
+  }
+
+  const handleForm = async (data:FormData) => {
+      const formData:Record<string, any> = Object.fromEntries(data);
+      console.log(formData);
+      setCount(prevState => prevState+1)
+      await talkAction(formData.text);
+      if (formRef) {
+        formRef.current?.reset();
+      }
+  }
+  const handleForm2 = async (data:FormData) => {
+    const formData:Record<string, any> = Object.fromEntries(data);
+    console.log(formData);
+    setCount(prevState => prevState+1)
+    await talkAction2(formData.text);
+    if (formRef2) {
+      formRef2.current?.reset();
+    }
   }
 
 
@@ -54,6 +98,34 @@ function App() {
             }} className={"rounded-lg bg-blue-400 text-white py-2 px-4 font-bold hover:bg-blue-500 active:bg-blue-600"}>
               測試動作
             </button>
+          </div>
+          <div className={" mt-4 w-full flex justify-center items-center"}>
+            <form className="w-full flex justify-center" ref={formRef} onSubmit={async (e)=>{
+              e.preventDefault();
+              const formData = new FormData(e.currentTarget);
+              await handleForm(formData);
+            }}>
+              <input className={" rounded-lg p-4 border-grey-400 border-2"} name="text" defaultValue={""} placeholder={"講些話吧"}/>
+              <div className={"ml-4 flex  items-center"}>
+                <button type="submit" className={"rounded-lg bg-pink-300 text-white py-2 px-4 font-bold hover:bg-pink-400 active:bg-pink-500"}>
+                  送出訊息
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className={" mt-4 w-full flex justify-center items-center"}>
+            <form className="w-full flex justify-center" ref={formRef2} onSubmit={async (e)=>{
+              e.preventDefault();
+              const formData = new FormData(e.currentTarget);
+              await handleForm2(formData);
+            }}>
+              <input className={" rounded-lg p-4 border-grey-400 border-2"} name="text" defaultValue={""} placeholder={"講些話吧"}/>
+              <div className={"ml-4 flex  items-center"}>
+                <button type="submit" className={"rounded-lg bg-amber-400 text-white py-2 px-4 font-bold hover:bg-amber-500 active:bg-amber-600"}>
+                  送出訊息2
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </main>
